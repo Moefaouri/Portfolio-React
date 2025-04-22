a9bb920aa4755b5797469402b46f0f5776b7c387
Moefaouri <Mohammad.Elfauri>
2025-04-22 14:56:09 +0300

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/reset.css';
 import "@/styles/globals.css";
 import '../styles/media.css';
 
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
