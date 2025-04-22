e3123eea2d2f9f4ca2c0ee73e2305d56eef70c9c
Moefaouri <Mohammad.Elfauri>
2025-04-22 15:13:19 +0300

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles/reset.css';
 import "@/styles/globals.css";
 import '../styles/media.css';
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
